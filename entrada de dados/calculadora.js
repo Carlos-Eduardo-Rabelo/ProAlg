049bba1241a8c360ca7bce90f6dff356823df67d
Carlos Eduardo <79851820+Projetos-Carlos@users.noreply.github.com>
2021-10-26 09:38:54 -0300

    Correção no Java

--- entrada de dados/calculadora.js
+++ entrada de dados/calculadora.js
@@ -3,14 +3,14 @@ var num2 = parseInt(prompt('Digite o N°2'));
 
 var soma = num1 + num2;
 var subtração = num1 - num2;
-var multiplicação = num1 * num2;
-var divisão = num1 / num2;
+var multiplicacao = num1 * num2;
+var divisao = num1 / num2;
 
 document.getElementById('num1').innerHTML = num1
 document.getElementById('num2').innerHTML = num2
 document.getElementById('soma').innerHTML = soma
 document.getElementById('sub').innerHTML = subtração
-document.getElementById('multiplicação').innerHTML = multiplicação
-document.getElementById('divisão').innerHTML = divisão
+document.getElementById('multiplicacao').innerHTML = multiplicacao
+document.getElementById('divisao').innerHTML = divisao
 
 //console.log(soma);
